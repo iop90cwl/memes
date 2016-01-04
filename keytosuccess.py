632b623ff39955d93eb9d383e4ff670e4e512767
Christian <iop90cwl@charter.net>
2016-01-04 13:18:11 -0800

    Added keytosuccess.py

---
+++ keytosuccess.py
@@ -0,0 +1,24 @@
+
+#The following program is a DJ Khaled meme. It also demonstrates a while loop.
+x = 1
+lol = 0
+memecount = 0
+sleepinpepperoni = raw_input('The key to success is to enter an integer. ')
+while x == 1:
+	lol = 0
+	try:
+		maymay = int(raw_input('Another one. '))
+	except ValueError:
+		print('That was not an integer. You played yourself. ')
+		break
+		
+		
+#DerpCode
+
+#if lol == 1:
+#	print('You smart.')
+#	x = 1
+			
+#else:
+#	print('Congratulations, you played yourself.')
+#	x = 0
